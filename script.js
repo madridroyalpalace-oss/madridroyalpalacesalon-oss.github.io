6b1a7cb74b19462a4649db962995ec9d01bf5445
madridroyalpalace-oss <madridroyalpalace@gmail.com>
2026-04-20 20:47:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// NAV cambia estilo al hacer scroll
+window.addEventListener("scroll", function () {
+  const nav = document.querySelector(".nav");
 
+  if (window.scrollY > 80) {
+    nav.style.background = "rgba(0,0,0,0.6)";
+    nav.style.backdropFilter = "blur(10px)";
+  } else {
+    nav.style.background = "transparent";
+    nav.style.backdropFilter = "none";
+  }
+});
